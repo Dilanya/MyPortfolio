6ec0658e5e06dc2fa9471fdfc33e1713acf7dff9
Dilanya <96423@fhss.sjp.ac.lk>
2023-04-10 23:33:54 +0530

    updated

--- src/components/index.js
+++ src/components/index.js
@@ -3,9 +3,9 @@ import Hero from './Hero';
 import Navbar from './Navbar';
 import About from './About';
 import Tech from './Tech';
-import Experience from './Experience';
+import Education from './Education';
+import Certifications from './Certifications';
 import Works from './Works';
-import Feedbacks from './Feedbacks';
 import Contact from './Contact';
 
 export {
@@ -13,9 +13,9 @@ export {
   Navbar,
   About,
   Tech,
-  Experience,
+  Education,
+  Certifications,
   Works,
-  Feedbacks,
   Contact,
   EarthCanvas, 
   BallCanvas, 
