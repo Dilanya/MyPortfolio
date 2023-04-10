4edc2dec15d2e2bf3c3d6faa52aa6e6d03affbbc
Dilanya <96423@fhss.sjp.ac.lk>
2023-04-10 23:34:20 +0530

    updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { motion } from 'framer-motion';
 import {styles} from '../styles';
-import {ComputersCanvas} from './canvas';
+import {StarsCanvas} from './canvas';
 
 const Hero = () => {
   return (
@@ -23,7 +23,7 @@ const Hero = () => {
         </div>
         
       </div>
-      <ComputersCanvas/>
+      <StarsCanvas/>
 
       <div className='absolute xs:bottom-10 bottom-32 
       w-full flex justify-center items-center'>
