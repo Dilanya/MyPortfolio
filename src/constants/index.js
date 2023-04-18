a384f0648803e550f65535db0e0c71953b5b0df2
Dilanya <96423@fhss.sjp.ac.lk>
2023-04-18 19:53:48 +0530

    constatnts changaed

--- src/constants/index.js
+++ src/constants/index.js
@@ -16,10 +16,6 @@ import {
     git,
     figma,
     docker,
-    meta,
-    starbucks,
-    tesla,
-    shopify,
     carrent,
     jobit,
     tripguide,
@@ -28,7 +24,9 @@ import {
     Hackerrank,
     dl,
     opencv,
-    comptia
+    comptia,
+    chess,
+    hotel
     
     
   } from "../assets";
@@ -122,60 +120,7 @@ import {
     },
   ];
   
-  const experiences = [
-    {
-      title: "React.js Developer",
-      company_name: "Starbucks",
-      icon: starbucks,
-      iconBg: "#383E56",
-      date: "March 2020 - April 2021",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
-    {
-      title: "React Native Developer",
-      company_name: "Tesla",
-      icon: tesla,
-      iconBg: "#E6DEDD",
-      date: "Jan 2021 - Feb 2022",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
-    {
-      title: "Web Developer",
-      company_name: "Shopify",
-      icon: shopify,
-      iconBg: "#383E56",
-      date: "Jan 2022 - Jan 2023",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
-    {
-      title: "Full stack Developer",
-      company_name: "Meta",
-      icon: meta,
-      iconBg: "#E6DEDD",
-      date: "Jan 2023 - Present",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
-  ];
+  
   
 
   const education = [
@@ -229,7 +174,7 @@ import {
       link: "https://www.linkedin.com/learning/certificates/718f12b753a89cbbedea41876a4c0ca8b88c1d5420f05da2bc7bc2a3764e621e",
     },
     {
-      name: "Mobile Networking",
+      name: "Mobile Networking         ",
       image:comptia,
       link: "https://www.linkedin.com/learning/certificates/e46aabab5fe1836bb9fff370b909299d121a34214e3a80f000dd32c2df904f59",
     },
@@ -240,9 +185,30 @@ import {
   
   const projects = [
     {
-      name: "Car Rent",
+      name: "Chess Pieces Detection",
       description:
-        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+        "This is an object detection model that built for Chess Pieces Detection. for this project I captured 150 images and then annotated them using Roboflow.",
+      tags: [
+        {
+          name: "object-detection",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "YoloV8",
+          color: "green-text-gradient",
+        },
+        {
+          name: "ML",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: chess,
+      source_code_link: "https://github.com/Dilanya/Chess-Pieces-Detection",
+    },
+    {
+      name: "Hotel Website",
+      description:
+        "Web-based platform that allows users to search, book, and manage reservations in the hotel, MERN stack used for the development",
       tags: [
         {
           name: "react",
@@ -253,36 +219,15 @@ import {
           color: "green-text-gradient",
         },
         {
-          name: "tailwind",
+          name: "bootstrap",
           color: "pink-text-gradient",
         },
       ],
-      image: carrent,
+      image: hotel,
       source_code_link: "https://github.com/",
     },
     {
-      name: "Car Rent",
-      description:
-        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
-      tags: [
-        {
-          name: "react",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "mongodb",
-          color: "green-text-gradient",
-        },
-        {
-          name: "tailwind",
-          color: "pink-text-gradient",
-        },
-      ],
-      image: carrent,
-      source_code_link: "https://github.com/",
-    },
-    {
-      name: "Job IT",
+      name: "Healthcare Mobile App",
       description:
         "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
       tags: [
@@ -325,4 +270,4 @@ import {
     },
   ];
   
-  export { services, technologies, experiences, projects, education, certificates };
+  export { services, technologies,  projects, education, certificates };
